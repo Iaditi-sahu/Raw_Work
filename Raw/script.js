e2ea88463afc4fd9fbc99645c1370127427c7d89
Aditi Sahu <aditisahu193@gmail.com>
2021-04-30 21:04:26 +0530

    init

--- Raw/script.js
+++ Raw/script.js
@@ -1,14 +1,14 @@
 let input = document.querySelector(".input_box");  
 let ul = document.querySelector(".task-list");
-input.addEventListener("keydown", function(elem) {  
+input.addEventListener("keydown", function(e) {  
     //  listeners to events here elem is object
-    console.log("event object", elem);
-    if(elem.key == "Enter"){
+    console.log("event object", e);
+    if(e.key == "Enter"){
         let task = input.value;
         let li = document.createElement("li");
         li.innerText = task;
 // it will add task value to newly created li
-        li.addEventListener("dblclick", function(elem){
+        li.addEventListener("dblclick", function(e){
             // double click listener
             li.remove();
             // remove or delete
